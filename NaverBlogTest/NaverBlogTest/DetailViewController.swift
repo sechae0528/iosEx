e42e3d821bb02cce49123a64ed451fafdd7d6e60
sungeun <sechae0528@gmail.com>
2018-10-05 11:25:41 +0900

    .

---
+++ NaverBlogTest/NaverBlogTest/DetailViewController.swift
@@ -0,0 +1,45 @@
+//
+//  DetailViewController.swift
+//  NaverBlogTest
+//
+//  Created by 스마트금융5 on 2018. 6. 1..
+//  Copyright © 2018년 cse. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = detailItem {
+            if let label = detailDescriptionLabel {
+                label.text = detail.timestamp!.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        configureView()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    var detailItem: Event? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
+    }
+
+
+}
+
